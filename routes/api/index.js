b1d31544503969422f5011a82403c1fcdae04a07
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-08-10 00:24:33 -0400

    finished github issue 2

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
+
+const commentRoutes = require("./comment-routes");
 const pizzaRoutes = require("./pizza-routes");
 
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+// add prefix of `/pizzas` to routes created in `pizza-routes.
+router.use("/comments", commentRoutes);
 router.use("/pizzas", pizzaRoutes);
 
 module.exports = router;
